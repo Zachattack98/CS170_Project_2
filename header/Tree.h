1aa0cc7c2638564766afbc8f2eeb43a37244f3e7
Zachattack98 <bakugan0498@yahoo.com>
2021-05-16 04:15:01 -0700

    Update Tree.h

--- header/Tree.h
+++ header/Tree.h
@@ -1,1 +1,26 @@
-#include
+#ifndef TREE_H_
+#define TREE_H_
+
+#include <bits/stdc++.h>
+#include "Node.h"
+#define N 3
+
+using namespace std;
+
+//creating a Depth-First Search Tree containing all possible states until the goal state is reached
+
+class Tree {
+  public:
+    Node* root;
+    stack<Node*> frontier;
+
+    //initialize the tree
+    Tree(Node* input_root) {
+        if (input_root == NULL)
+            return;
+        this->root = input_root;
+        frontier.push(this->root);
+    }
+};
+
+#endif
