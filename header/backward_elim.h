96429218f8e94ff651b9b9ffb0c982f0fedb89ee
Zachattack98 <bakugan0498@yahoo.com>
2021-05-16 19:10:14 -0700

    Update backward_elim.h

--- header/backward_elim.h
+++ header/backward_elim.h
@@ -1,1 +1,16 @@
-float P_value = 0.05;  //significance level (5%); used to find all features unneccsary ( > P_level)
+#ifndef BACKWARD_ELIM_
+#define BACKWARD_ELIM_
+
+#include <bits/stdc++.h>
+#include "Node.h"
+
+using namespace std;
+
+//Greedy Bachward Elimination
+class Eliminate {
+  public:
+    float P_value = 0.05;  //significance level (5%); used to find all features unneccsary ( > P_level)
+
+};
+
+#endif
