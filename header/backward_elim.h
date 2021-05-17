c38d86279a81e692d0c79e93786cad3c2ea9f341
Zachattack98 <bakugan0498@yahoo.com>
2021-05-16 20:08:56 -0700

    Provided base outputs of be_algorithm

--- header/backward_elim.h
+++ header/backward_elim.h
@@ -10,6 +10,7 @@ using namespace std;
 class Eliminate {
   public:
     float P_value = 0.05;  //step 1) significance level (5%); used to find all features unneccsary ( > P_level)
+    int eliminated; //totl number of features that were eliminated
   
     float Eliminate(Node* root, int features){
       Tree* tree = new Tree(root)
@@ -21,13 +22,25 @@ class Eliminate {
         }
       }
       
+      //show accuracy after using each feature individually
+      for (int i = 0; i < features; i++) {
+          cout << "\nFeature " << i << " shows a predictor value of " << tree->explored << "%";
+      }
+      
+      
       //step 3) check for the predictor/value of all nodes to see which is highest
+      
+      for (int j = 0; j < features; j++) {
+        cout << "\n\nFeature " << j << " is currently the best predictor, " << tree->explored << "%" ;
+      }
+      
       
       //step 4) if highest predictor is higher than P-value, then 
       //step 5) eliminate that feature from the stack/list and find next highest in step 3)
       
       //step 6) once highest predictor found is less than P-value, all remaining features return as valid
       
+      cout << "\n\nA total of " << eliminated << " features were eliminated! Leaving us with the following set: ";
       
     }
   
