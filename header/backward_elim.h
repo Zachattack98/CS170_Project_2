d73b03b3f3852abcbeea60049f79263c212b895d
Zachattack98 <bakugan0498@yahoo.com>
2021-05-23 01:23:54 -0700

    BE is operational; displays random accuracies for each new set, except FIRST accuracy is always the same negative

--- header/backward_elim.h
+++ header/backward_elim.h
@@ -1,68 +1,125 @@
 #ifndef BACKWARD_ELIM_H
 #define BACKWARD_ELIM_H
 
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
 #include "Node.h"
+#include "Tree.h"
 
 using namespace std;
 
-//Greedy Bachward Elimination
+//Greedy Forward Selection
 class Eliminate {
-  public:
-    int eliminated; //total number of features that were eliminated
-    vector<feature> bw_features; //new subset of features; does not contain any removed
-    char highest_accuracy_bw;
-    
-    void Eliminate(Node* root, char features, float accuracy){
-      Tree* tree = new Tree(root)
-        
-      //provide random values to all avalable combinations
-      for (int i = 0; i < features; i++) {
-        for (int j = 0; j < features; j++) {
-          if (i != 0 && j != 0)
-            tree->explored.push_back(accuracy[i][j])
-          //tree->explored.push_back(root->accuracy[i + j]);
-        }
-      }
-      
-      while(features != feature) {
+public:
+    Node* root;
+    vector<char> feats = { 'a', 'b', 'c', 'd' };
+    float P_value = 0.05;  //step 1) significance level (5%); used to find all features unneccsary ( > P_level)
+    int eliminated = 0; //total number of features that were eliminated
+    //int accuracy_count = 0; //index that represents the accuracy chosen in the lit of randomly generated accuracies
 
-        if(features > feature) {
-          addFeatures(features);
-        }
-
-        else if(features < f_init) {
-          removeFeatures(features);
-        }
-
-      }
-
-      //begin going down the tree from root
-      while(features) {
-        int j = 0;
-        
-        //displaying accuracy of each subset
-        for (int i = 0; i < features; i++) {
-          cout << "\nFeature(s) " << feature(i) << " shows an accuracy value of " << tree->explored[0][i] << "%";
-          
-          //if you find a higher accuracy, make that node the new parent
-          if(i != 0 && (tree->explored[0][i-1] < tree->explored[0][i])) {
-            root->parent.at(i);
-
-            //pop the last subset then include the new (highest) subset
-            bw_features.pop();
-            bw_features.push_back(root->children.at(i));
-          }
-
-        }
-
-        features--; 
-        j++;
-      }
-        
-      eliminated = (int)features - int(bw_features);
-
-      cout << "\n\nA total of " << eliminated << " features were eliminated! Leaving us with the following set: ";
+    Eliminate() {
+        root = new Node();
     }
-  
+
+    void backwardEliminate() {
+        Tree* tree = new Tree(root);
+        parentSet(tree);
+        removeFeature(tree);
+
+        for (int i = 0; i < 3; i++) {
+            parentSet(tree);
+            removeFeature(tree);
+        }
+        return;
+    }
+
+    void removeFeature(Tree* tree) {
+        Node* currNode = tree->currNode;
+        for (int i = 0; i < 4 - currNode->curr_features.size(); i++) {
+            if (currNode->children.at(i)->curr_features.at(0) == 'b') {
+                tree->currNode = currNode->children.at(i);
+                cout << "feature(s) removed: ";
+                eliminated++;
+                for (int j = 0; j < tree->currNode->curr_features.size(); j++)
+                    cout << tree->currNode->curr_features.at(j) << " ";
+
+                Node* accuracyNode = new Node(currNode);
+
+                cout << "\nAccuracy of new set: " << accuracyNode->accuracy << "%";
+                cout << "\n\n";
+
+                //accuracy_count++;
+
+                return;
+            }
+        }
+    }
+
+    void parentSet(Tree* tree) {
+        int feature_size;
+        vector<char> new_features;
+        Node* currNode = tree->currNode;
+        cout << "size: " << 4 - tree->currNode->curr_features.size() << endl;
+        feature_size = 4 - tree->currNode->curr_features.size();
+
+
+        if (feature_size != 0) {
+            new_features = validFeatures(currNode, feature_size);
+            cout << "current parent node: ";
+            for (int i = 0; i < feature_size; i++) {
+                Node* newNode = new Node(tree->currNode);
+                newNode->addFeature(new_features.at(i));
+                cout << new_features.at(i) << " ";
+                currNode->children.push_back(newNode);
+            }
+            cout << "\n";
+
+            /*cout << "child node chosen: ";
+            for (int i = 0; i < feature_size-1; i++) {
+                Node* newNode = new Node(tree->currNode);
+                newNode->addFeature(new_features.at(i));
+                cout << new_features.at(i) << " ";
+                currNode->children.push_back(newNode);
+            }
+            cout << "\n";*/
+        }
+    }
+
+    vector<char> validFeatures(Node* currNode, int feature_size) {
+        vector<char> new_feature;
+        vector<char> invalid_feats;
+        if (feature_size == 4) {
+            for (int i = 0; i < 4; i++)
+                new_feature.push_back(feats.at(i));
+        }
+        else if (feature_size == 3) {
+            invalid_feats.push_back(currNode->curr_features.at(0));
+            for (int i = 0; i < 4; i++) {
+                if (invalid_feats.at(0) != feats.at(i))
+                    new_feature.push_back(feats.at(i));
+            }
+        }
+        else if (feature_size == 2) {
+            invalid_feats.push_back(currNode->curr_features.at(0));
+            invalid_feats.push_back(currNode->curr_features.at(1));
+            for (int i = 0; i < 4; i++) {
+                if (invalid_feats.at(0) != feats.at(i) && invalid_feats.at(1) != feats.at(i))
+                    new_feature.push_back(feats.at(i));
+            }
+        }
+        else if (feature_size == 1) {
+            invalid_feats.push_back(currNode->curr_features.at(0));
+            invalid_feats.push_back(currNode->curr_features.at(1));
+            invalid_feats.push_back(currNode->curr_features.at(2));
+            for (int i = 0; i < 4; i++) {
+                if (invalid_feats.at(0) != feats.at(i) && invalid_feats.at(1) != feats.at(i) && invalid_feats.at(2) != feats.at(i))
+                    new_feature.push_back(feats.at(i));
+            }
+        }
+        else {
+            cout << "feature size: " << feature_size << endl;
+        }
+        return new_feature;
+    }
 };
+
+#endif
