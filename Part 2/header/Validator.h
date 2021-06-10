3ea1c52ff0e36a0aeed9e069233e905f4bda7541
Zachattack98 <bakugan0498@yahoo.com>
2021-06-10 03:22:44 -0700

    Included necessary elements, still needs testing

--- Part 2/header/Validator.h
+++ Part 2/header/Validator.h
@@ -22,9 +22,6 @@ class Validator {
     int leave_one_out_validation (vector<int> subset, string file_choice) {
         int correct_predict_cnt;    //keep track of all correct predictions
         ifstream myfile(file_choice);   //open file; read from the appropriate file
-        
-        //while(getline(myfile, line))    //while there is still lines to read 
-            //++num_lines;
 
         //transfer all inputted features into feat_subset
         for(int i = 0; i < subset.size(); i++)
@@ -35,7 +32,7 @@ class Validator {
         //Read all ground truth labels
         vector<vector<int>> ground_truth_label;
         
-        fstream myfile;
+        ifstream myfile;
         myfile.open(file_choice);   //open file; read from the appropriate file
           
         string line;
@@ -63,25 +60,31 @@ class Validator {
         
         ValidTimer t;
         correct_predict_cnt = 0;
+        float test_instance_ID = 0;
+        float train_instances = 0;
 
         //timer start
 		printf("Starting validation...");
 		t.start();
         for(int i = 0; i < instance_ID.size(); i++) {
-            vector<vector<float>> test_instance_ID = instance_ID;
-            //train_instances = all other instances excluding test_instance
-            //call Classifier.Test() and pass this test_instance_ID to it. It returns predicted_label
+            for(int j = 0; j < subset.size(); j++) {
+                test_instance_ID = instance_ID[feat_subset[j]-1][j];
+            }
 
-            /*if(predicted_label[i] == ground_truth_label[i]) {
+            train_instances = test_instance_ID - feat_subset[i];
+
+            Classifier* test = new Test(test_instance_ID);
+
+            if(test.predicted_label[i] == ground_truth_label[i]) {
                 correct_predict_cnt++;
-            }*/
+            }
         }
 
         //timer stop
         t.stop();
 		printf("Time taken for validation = %0d ms", t.getTime());
 
-		//accuracy_scr  = correct_predict_cnt / total number of instances
+		accuracy_scr  = correct_predict_cnt / (test_instance_ID + train_instances);
 		printf("Using features {%0d}, the accuracy is {%0f}", feat_subset, accuracy_scr);
 		
         return accuracy_scr;
