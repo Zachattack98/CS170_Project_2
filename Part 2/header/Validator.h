6f763a60dc96221645aac5c0d6c41782fd7908ec
Zachattack98 <bakugan0498@yahoo.com>
2021-06-11 06:48:45 -0700

    Provided lines for default rate

--- Part 2/header/Validator.h
+++ Part 2/header/Validator.h
@@ -19,7 +19,7 @@ class Validator {
         string line;
         int label;
 
-    int leave_one_out_validation (vector<int> subset, string file_choice) {
+    int leave_one_out_validation (vector<int> subset, string file_choice, int algorithm) {
         int correct_predict_cnt;    //keep track of all correct predictions
         ifstream myfile(file_choice);   //open file; read from the appropriate file
 
@@ -34,11 +34,8 @@ class Validator {
         
         Dataset* parse = new Dataset();
     
-        parse->RowsandColumns("large.txt");
-        parse->Parser("large.txt");
-    
-        cout << parse->rows << endl;
-        cout << parse->cols << endl;
+        parse->RowsandColumns(file_choice);
+        parse->Parser(file_choice);
     
         for(int i = 0; i < parse->rows; i++) 
             ground_truth_label.push_back(parse->data[i][0]);  
@@ -61,6 +58,25 @@ class Validator {
 	    printf("Starting validation...");
 	    t.start();
 
+        //calculate default rate; only for forward selection
+        /*if(algorithm == 1) {
+            for(int i = 0; i < parse->rows; i++) {
+                if(ground_truth_label[i] == 1)
+                    cnt2++;
+                else if(ground_truth_label[i] == 2)
+                    cnt3++;
+            }
+
+            if(cnt2 < cnt3) {
+                correct_predict_cnt = cnt2;
+            }
+            else {
+                correct_predict_cnt = cnt3;
+            }
+            //Note: if both counters are the same we can use either, here we will use else
+        }*/
+
+        //else {
         for(int i = 0; i < parse->rows; i++) {
             if((feat_subset[cnt]-1) == i){
                 for(int j = overall_cnt; j < ((parse->cols - 1) + overall_cnt); j++) {
@@ -84,7 +100,7 @@ class Validator {
                 correct_predict_cnt++;
             }
         }
-
+        //}
         //timer stop
         t.stop();
 	    printf("Time taken for validation = %0d ms", t.getTime());
