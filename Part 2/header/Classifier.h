93826e1365f06194c6a2f63c37970d4e72049aed
Zachattack98 <bakugan0498@yahoo.com>
2021-06-11 22:11:02 -0700

    Included Classifier; trouble calling the Test function and getting predicted label

--- Part 2/header/Classifier.h
+++ Part 2/header/Classifier.h
@@ -2,63 +2,61 @@
 #define CLASSIFIER_H_
 
 //#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
 #include <ctime>
 #include <time.h>
+#include "Dataset.h"
 
 class Classifier
 {
-	
-	function Train(...)
-	{
-		
-		//inputs: 1) train_instances : M vectors each of size n where n = no. of features per instance, M = no. of total instances
-    int num_feats_instance;   //number of features per instance
-    vector<num_feats_instance> tot_instances; //number of total instances
-    
-		//2) train_labels : a list or 1-d vector or array of length M where train_labels[i] = labels of ith instance
-		int train_labels[tot_instances];
-    
-    //OR
-		//inputs: 1) train_instance_IDs
-		
+public:
+        int predicted_label[41];
+	Dataset* training_data;
+	int feature_size;
+	vector<int> feature_indexes;
+        
+        
+	void Train(Dataset* data, vector<int> &features) {
+	    training_data = data;
+	    feature_size = features.size();
+        feature_indexes = features;
 		//outputs: None
 				//NN classifier isn't a real ML classifier so there is no model for the Train method to build using (train_instances, train_labels) and return.
 				//I am guessing that's why the professor said there is no output for this method in the part 2 google doc.
 
 	}
 
-	function Test(test_instance <or test_instance_ID>)
+	int Test(int testing_instance)
 	{
-		//inputs: 1) test_instance: this is either the n-dimensional vector representing your test instance or just the unique ID or index of your test instance.
-		
-    //output: 1) predicted_label: the predicted label for this test_datapoint using the nearest neighbor classifer.
-    int predicted_label;
-    
-		//Step 1: Get n-dimensional the test instance (if only ID given as input)
-		
-    
-    //Step 2: Compute its distance from all train instances (you will need to produce a M-1 x 3 table like {instance_ID, distance_from_test_instance, label} at this step)
+	    //int predicted_label;
+	    double distance, shortest_class;
+	    double shortest_distance = 1000;
+	    double data[1][feature_size + 1];
 
+	    for (int i = 0; i < feature_size; i++)
+            data[0][i] = training_data->data[testing_instance][feature_indexes.at(i)];
 
-		//Option 1: Maybe use Classifier.Train() to get the actual train_instances and train_labels by just passing Classifier.Train() the list of train_instance_IDs?
-		//(Note train_instance_IDs = all_IDs - test_instance_ID)
-		//Then compute the M-1 x 3 table right here?
-		
-    //Option 2: Maybe use Classifier.Train() to actually compute all the distances too and return this M-1 x 3 table? 
-		//(Note: By return I technically dont mean return, it could be a global M-1 x 3 table that just gets updated at every iteration such that you dont pass too much data around.)
-		//(This would also probably follow the professors logic of Classifier.Train() not having any output... I dont know...)
-		
-    //Option 3: You could just read the data file and do everything right here eliminating the need for a Classifier.Train()? Since, you know, we arent training anything. 
-		//I dont know...ask the professor please.
-
-		
-    
-    //Step 3: Sort that M-1 x 3 table by distance_from_test_instance (ascending)
-		
-    //Step 4: predicted_label = label of first training instance in the sorted table.
-
-		return predicted_label
+        for (int i = 0; i < training_data->data_size; i++) {
+            distance = 0;
+            if (i == testing_instance)
+                continue;
+            for (int j = 0; j < feature_size; j++)
+                distance += pow(data[0][j] - training_data->data[i][feature_indexes.at(j)], 2);
+            distance = sqrt(distance);
+            if (distance < shortest_distance) {
+                shortest_distance = distance;
+                shortest_class = training_data->data[i][0];
+            }
+            cout << training_data->data[i][0]<< ":" << distance << " ";
+        }
+          
+        predicted_label = shortest_class;
+        cout << "\n\nShortest distance: " << shortest_distance << endl;
+        
+        cout << "Predicted Label: ";
+        return predicted_label;
 	}
-	
-}
+};
+
+#endif
